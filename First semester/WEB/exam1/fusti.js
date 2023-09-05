51f87cbf3759408d660b6082568c462f02e12e86
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2023-09-05 18:34:13 +0200

    Some changes

--- First semester/WEB/exam1/fusti.js
+++
@@ -1,6 +0,0 @@
-function mouseOver(img) {
-    document.getElementById(img).style.width = "400px";
-    document.getElementById(img).addEventListener("mouseout", function (){
-        document.getElementById(img).style.width = "";
-    })
-}
