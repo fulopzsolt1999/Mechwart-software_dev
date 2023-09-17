790402412a2d58c8c12df576faf2063ef7c98465
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2023-09-17 11:45:20 +0200

    Ready to review.
    media queries added,
    some errors debugged,
    comments added

--- Second semester/Web-dev/2023-09-12/index.js
+++ Second semester/Web-dev/2023-09-12/index.js
@@ -1,6 +1,13 @@
+/* Get the name label to deal with the empty input error message. */
 var nameLabel = document.getElementById("name-label");
+
+/* Get the phone format propertie to deal with the phone number error messages. */
 var phoneFormat = document.getElementById("phone-format");
+
+/* This is need to check the email correction. */
 var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+/* These are the input fields. */
 var regForm = {
   name: document.getElementById("name"),
   birth: document.getElementById("birth"),
@@ -11,14 +18,18 @@ var regForm = {
   confPw: document.getElementById("confPassword"),
 };
 
-const date = new Date();
-let currentDay = String(date.getDate()).padStart(2, "0");
-let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
-let currentYear = date.getFullYear();
+/* Get the current date to show it automaticly on the website. */
+function get_current_date() {
+    const date = new Date();
+    let currentDay = String(date.getDate()).padStart(2, "0");
+    let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
+    let currentYear = date.getFullYear();
+    regForm.birth.value = `${currentYear}-${currentMonth}-${currentDay}`;
+}
 
-regForm.birth.value = `${currentYear}-${currentMonth}-${currentDay}`;
-
+/* After clicking the button this function deals with empty fields, error messages. */
 function registration_form() {
+    get_current_date();
     if (nameLabel.previousSibling != document.getElementById("form-title")) {
         nameLabel.previousSibling.remove();
     }
@@ -46,15 +57,24 @@ function registration_form() {
     check_empty_form();
 }
 
+/*
+    If the input field's border style was already red because of some error,
+    the user clicks in it than restore the border style
+*/
 function check_input_field(field){
-    regForm[field].style.border = "1px solid #979797";
+    if (regForm[field].style.borderTop == "1px solid red") {
+        regForm[field].style.borderTop = "1px solid #faebd7";
+        regForm[field].style.borderBottom = "1px solid #faebd7";
+    }
 }
 
+/* If the user let any input field empty, this function will highlite the input field's border red. */
 function check_empty_form() {
     let error = false;
     for (let data in regForm) {
         if (regForm[data].value == "") {
-            regForm[data].style.border = "1px solid red";
+            regForm[data].style.borderTop = "1px solid red";
+            regForm[data].style.borderBottom = "1px solid red";
             error = true;
       }
     }
@@ -64,6 +84,7 @@ function check_empty_form() {
     check_correct_inputs();
 }
 
+/* This function deals with the inputs correction. */
 function check_correct_inputs() {
     if (!regForm.name.value.includes(" ")) {
         error_message("name", document.createTextNode("Please enter a valid name!"));
@@ -74,7 +95,8 @@ function check_correct_inputs() {
     }
     if (regForm.phone.value.slice(0, 2) != "06" && regForm.phone.value.slice(0, 2) != "36" || regForm.phone.value.length != 11) {
         let phoneErr = document.createTextNode("Please enter a valid phone number");
-        regForm.phone.style.border = "1px solid red";
+        regForm.phone.style.borderTop = "1px solid red";
+        regForm.phone.style.borderBottom = "1px solid red";
         phoneFormat.parentNode.insertBefore(phoneErr, phoneFormat.nextSibling);
     }
     if (!regForm.email.value.match(validRegex)) {
@@ -93,12 +115,15 @@ function check_correct_inputs() {
     }
 }
 
+/* This function writes the empty field error message. */
 function empty_message() {
     let emptyMessage = document.createTextNode("Please fill out everything!");
     nameLabel.parentNode.insertBefore(emptyMessage, nameLabel);
 }
 
+/* This function writes the error messages for all of the input fields. */
 function error_message(inpField, errMessage) {
-    regForm[inpField].style.border = "1px solid red";
+    regForm[inpField].style.borderTop = "1px solid red";
+    regForm[inpField].style.borderBottom = "1px solid red";
     regForm[inpField].parentNode.insertBefore(errMessage, regForm[inpField].nextSibling);
 }
