eb71742a8e9e425981811aa0ed81163afbcfcd5d
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2023-09-23 09:06:06 +0200

    Some progress

---
+++ Second semester/C#/Basics-C#/CelsiusToFaren/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelsiusToFaren
+{
+    internal class Program
+    {
+        public void weather(string cOrF, double n)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("°C vagy °F?: ");
+                string celOrFar = Console.ReadLine();
+                Console.Write("Hány fok van?: ");
+                double num = double.Parse(Console.ReadLine());
+                if (celOrFar == "c" || celOrFar == "C" || celOrFar == "°C")
+                {
+                    weather("C", num);
+                    Console.Write($"Ez fahreinheit-ben ennyi(°F): {Math.Round(num * 1.8 + 32)}");
+                    break;
+                }
+                else if (celOrFar == "f" || celOrFar == "F" || celOrFar == "°F")
+                {
+                    Console.Write($"Ez fahreinheit-ben ennyi(°F): {Math.Round((num - 32) / 1.8)}");
+                    break;
+                }
+                else
+                {
+                    Console.Write("!!!c/C/°C vagy f/F/°F!!!");
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Second semester/C#/Basics-C#/TeglatestFelszTerf/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeglatestFelszTerf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Téglatest 3 oldala bekér, felszín és térfogat számítás
+            Console.Write("Add meg a téglatest a oldalát: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Add meg a téglatest b oldalát: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Add meg a téglatest c oldalát: ");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A téglatest térfogata: {Math.Round(a*b*c, 2)},\n" +
+                $"A téglatest térfogata: {Math.Round(2*(a*b+b*c+a*c), 2)}");
+
+            Console.ReadKey();
+        }
+    }
+}
