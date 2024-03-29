fa5af85f548504244319e42472c493f57c30fbfa
Fülöp Zsolt <fulopzsolt1999@gmail.com>
2024-03-29 11:05:12 +0100

    Props -Student- -PropTypes -Default props

--- Second and Third semester/Web-dev/React/Full course -2024-/my-react-app/src/App.jsx
+++ Second and Third semester/Web-dev/React/Full course -2024-/my-react-app/src/App.jsx
@@ -1,9 +1,13 @@
-import Card from "./Card/Card.jsx";
+import Student from "./Student/Student";
 
 function App() {
    return (
       <>
-         <Card />
+         <Student name="Spongebob" age={30} isStudent={true} />
+         <Student name="Patric" age={42} isStudent={false} />
+         <Student name="Squidward" age={50} isStudent={false} />
+         <Student name="Sandy" age={27} isStudent={true} />
+         <Student />
       </>
    );
 }
